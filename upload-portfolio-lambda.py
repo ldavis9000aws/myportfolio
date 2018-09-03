e970e931518549cc0ffe7be8ce81f6c773df8f2e
Lawrence Davis <ldavis9000aws@gmail.com>
2018-09-03 19:43:41 -0400

    Updated Lambda function with exceptions for error handling

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,19 +5,31 @@ import mimetypes
 
 def lambda_handler(event, context):
     
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:344267670530:DeployPortfolioTopic')
     
-    portfolio_bucket = s3.Bucket('portfolio.lawrencepdavisjr.com')
-    build_bucket = s3.Bucket('portfoliobuild.lawrencepdavisjr.com')
-    
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm, 
-              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    
+    try: 
+        s3 = boto3.resource('s3')
+        
+        portfolio_bucket = s3.Bucket('portfolio.lawrencepdavisjr.com')
+        build_bucket = s3.Bucket('portfoliobuild.lawrencepdavisjr.com')
+        
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, 
+                  ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        print "Job Done!"
+        topic.publish(Subject="Portfolio Deployed!", Message="Portfolio Deployed Sucessfully!")
+    except: 
+        topic.publish(Subject="Portfolio Failed Deployment!", Message="The Portfolio Deployment Failed!")
+        raise
+  
     return 'Hello From Lambda'
+    
+    
